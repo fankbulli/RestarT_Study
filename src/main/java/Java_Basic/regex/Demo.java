820c16aaf6e94242ec642548d17ebb9debde0730
fankbulli <18000252855@163.com>
2020-07-14 16:21:54 +0800

    HashMap

---
+++ src/main/java/Java_Basic/regex/Demo.java
@@ -0,0 +1,11 @@
+package Java_Basic.regex;
+
+/**
+ * @program: Restart_Study
+ * @description: 正则的样例类
+ * @author: zhongming
+ * @time: 2020/6/9 8:54
+ */
+
+public class Demo {
+}
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,3 +1,9 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 
 /**
@@ -19,7 +25,35 @@ public class Test {
     }
     HashMap hashMap=new HashMap();
 
+
     public static void main(String[] args) {
         System.out.println(tableSizeFor(79));;
+
+        Calendar cal=Calendar.getInstance();
+        int day=cal.get(Calendar.DAY_OF_MONTH);
+        System.out.println(day);
+
+
+
     }
+//    public static void main(String[] args) {
+//        //1、普通的时间转换
+//        String string = new SimpleDateFormat("yyyy-MM-dd").format(new Date()).toString();
+//        System.out.println(string);
+//        System.out.println("-------------------------------");
+//        //2、日历类的时间操作
+//        Calendar calendar = Calendar.getInstance();
+//        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+//        String d="2038-01-01";
+//        try {
+//            Date date=sdf.parse(d);
+//            System.out.println(System.currentTimeMillis());
+//            System.out.println(date.getTime());
+//            calendar.setTime(date);
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println();
+//    }
+
 }
